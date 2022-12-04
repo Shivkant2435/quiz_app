7c5b0f640b86656b527e5f8e6dd533903bf36b08
Your Name <you@example.com>
2022-12-05 02:22:40 +0530

    made some changes in background

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // test
+function getColor(){
+    const randomNumber= Math.floor(Math.random()*16777215);
+    
+    const randomcode="#"+randomNumber.toString(16);
+    
+    document.body.style.backgroundColor=randomcode;
+
+}
+setInterval(getColor,2000);
