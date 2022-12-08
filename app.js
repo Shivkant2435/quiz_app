da40d404663111c6014aeddf4284132ea83134dd
Your Name <you@example.com>
2022-12-08 19:46:20 +0530

    changes_made_in_app

--- app.js
+++ app.js
@@ -5,6 +5,49 @@ function getColor(){
     const randomcode="#"+randomNumber.toString(16);
     
     document.body.style.backgroundColor=randomcode;
-
+    
+    
 }
 setInterval(getColor,2000);
+const questions= [{
+    question : "Which of the following is a client site language?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d",
+},
+{
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "a",
+},
+{
+    question: "What year was JavaScript launched?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "none of the above",
+    correct: "b",
+},
+{
+    question: "What does CSS stands for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "b",
+}
+];
+let index=0;
+const quesBox = document.getElementById("quesbox");
+ const loadques = () =>{
+ const data = questions[index];
+ console.log(data);
+ quesBox.innerText = '${index+1} ${data.question}';
+ 
+}
+loadques();
